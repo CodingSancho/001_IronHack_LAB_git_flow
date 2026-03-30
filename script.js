e2fdacc1df0a560338c7100032605d2a9592d465
Sancho <lantosviktor22@gmail.com>
2026-03-30 16:07:30 +0200

    feat: add theme switch button to header

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("JS is connected! 🚀");
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  const isDark = document.body.getAttribute("data-theme") === "dark";
+  document.body.setAttribute("data-theme", isDark ? "light" : "dark");
+  toggle.textContent = isDark ? "🌙" : "☀️";
+});
