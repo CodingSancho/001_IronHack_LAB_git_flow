75a5d00c7755ade1ea0610b1186ddb4e34b3f35e
Sancho <lantosviktor22@gmail.com>
2026-03-30 17:40:35 +0200

    changed expected icon to moon on dark mode

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-console.log("JS is connected! 🚀");
-
 const toggle = document.getElementById("themeToggle");
+
+document.body.setAttribute("data-theme", "dark");
+toggle.textContent = "🌙";
 
 toggle.addEventListener("click", () => {
   const isDark = document.body.getAttribute("data-theme") === "dark";
   document.body.setAttribute("data-theme", isDark ? "light" : "dark");
-  toggle.textContent = isDark ? "🌙" : "☀️";
+  toggle.textContent = isDark ? "☀️" : "🌙";
 });
